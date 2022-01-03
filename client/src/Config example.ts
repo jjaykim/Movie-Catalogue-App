fb1b2c37ba03a95c2f8dbab920ad749ed2fa0383
jjaykim <dnglglmm@gmail.com>
2022-01-03 12:26:32 -0500

    change gitignore file

---
+++ client/src/Config example.ts
@@ -0,0 +1,11 @@
+export const API_KEY = 'Enter Your personal Key';
+
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+export const UPCOMING_URL = `${API_URL}movie/upcoming?api_key=${API_KEY}&page=`;
+
+export const SEARCH_URL = `${API_URL}search/movie?${API_KEY}&`;
